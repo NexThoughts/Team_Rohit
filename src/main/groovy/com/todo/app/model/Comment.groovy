6495e546be4e475450f5572a6b4f8e699dca8088
abhinav <abhinav.sri2009@gmail.com>
2017-12-16 19:25:16 +0530

    added fields in the comment domain

--- src/main/groovy/com/todo/app/model/Comment.groovy
+++ src/main/groovy/com/todo/app/model/Comment.groovy
@@ -7,11 +7,14 @@ import io.vertx.ext.auth.User
 class Comment {
     String description
     String id
+    User createdBy
+    Task task
 
 
-    Comment(){}
-    Comment (JsonObject jsonObject){
-          this.description=jsonObject.getString("description")
-        this.id=jsonObject.getString("_id")
+    Comment() {}
+
+    Comment(JsonObject jsonObject) {
+        this.description = jsonObject.getString("description")
+        this.id = jsonObject.getString("_id")
     }
 }
